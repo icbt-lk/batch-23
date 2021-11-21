2c074dfef4b45aa1014cb23096dc214e1972cbf5
Tisal Thenuwara <tisal396@gmail.com>
2021-11-21 14:28:03 +0530

    Interface changes

--- Tisal/lecture01/src/main/java/com/toxic/lecture01/App.java
+++ Tisal/lecture01/src/main/java/com/toxic/lecture01/App.java
@@ -11,7 +11,7 @@ package com.toxic.lecture01;
 public class App {
     public static void main(String[] args)
     {
-    System.out.println("Hello World!");
+//    System.out.println("Hello World!");
     
     Student st = new Student();
     
