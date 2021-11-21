3b109c6a3ec4d1015c8ccc6d6514a5dd962c190d
Tharik Kanaka <tharik.kanaka@gmail.com>
2021-11-21 13:56:57 +0530

    Include initial project

---
+++ tharik/lecture02/src/main/java/newpackage/App.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author tharik
+ */
+public class App {
+    public static void main(String[] args) {
+        
+        Person p = new Person();
+        p.getFirstName();
+        p.getLastName();
+        
+        Lecturer l = new Lecturer("I001", "123V", "John", "Smith");
+        System.out.println(l.getFullName());
+        
+        Person p1;
+        
+        p1 = new Person();
+        p1 = new Student();
+        p1 = new Lecturer();
+    }
+}
