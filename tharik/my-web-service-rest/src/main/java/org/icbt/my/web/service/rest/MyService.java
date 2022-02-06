1141a3e084f98d0ca0799a4fc13afd4d0202fe52
Tharik Kanaka <tharik.kanaka@gmail.com>
2022-02-06 10:24:13 +0530

    Include REST service project

---
+++ tharik/my-web-service-rest/src/main/java/org/icbt/my/web/service/rest/MyService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.icbt.my.web.service.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author tharik
+ */
+@Path("/my-service")
+public class MyService {
+    @GET
+    public Response getMessage(){
+       return Response.status(200).entity("Hello World").build();
+    }
+}
